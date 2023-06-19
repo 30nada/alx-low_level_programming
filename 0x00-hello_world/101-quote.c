3b7bacf14d27940a754ccd6bbaa99f1fadb377c1
30nada <nada.saleh35735@gmail.com>
2023-06-19 16:39:53 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ *        write(int fd.const void "buf.size_t count)
+ *
+ *Return: 1 (fail)
+ */
+
+int main (void)
+{
+	char quo= "and that piece of art is useful" - Dora Korpar, 2015-10-19\n;
+
+        write (1, quo;
+	Return(1);
+}
+
+
