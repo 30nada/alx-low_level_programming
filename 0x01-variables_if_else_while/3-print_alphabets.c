1fd3e97d31e56c102ccaf66370082fd9eecb1b11
30nada <nada.saleh35735@gmail.com>
2023-08-15 01:42:43 +0300

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- Enty point
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+
+	 for (low = 'A'; low <= 'Z'; low++)
+		 putchar(low);
+	         putchar('\n');
+		 retuen (0);
+}
+
