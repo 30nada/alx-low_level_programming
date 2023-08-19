bf6069180e433bb4eb4cd584017f6178ffec062c
30nada <nada.saleh35735@gmail.com>
2023-08-18 19:00:43 -0700

    third

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: the numder since 0 up to 9
+ */
+
+void print_numbers(void);
+{
+	int c;
+        for  (c = 0; c <= 9; c++);
+	{
+	_putchar(x + '0');
+	}
+	_putchar('n');
+}
