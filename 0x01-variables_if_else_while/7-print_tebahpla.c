08cd7ec2f0477a49e4b64b8e92a3a41c51380df5
30nada <nada.saleh35735@gmail.com>
2023-10-06 17:44:25 -0700

    ./7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- Enty point
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
